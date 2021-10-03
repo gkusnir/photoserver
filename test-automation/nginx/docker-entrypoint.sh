123feba9e36c926793a3c5051a36a8695f513c04
Gustav Kusnir <gkusnir@arsec.sk>
2021-10-03 19:59:19 +0200

    i will just use jonasal/nginx-certbot:latest

--- test-automation/nginx/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# add crontab entry to renew the letsencrypt certificate
-#echo "adding crontab"
-#echo "0 23 * * * certbot renew --dry-run" | crontab -
-
-# start nginx
-#echo "starting nginx"
-#nginx
