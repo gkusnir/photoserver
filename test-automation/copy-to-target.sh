dfa505e04f27cf086a7b05aaad2f64783339546a
Gustav Kusnir <gkusnir@arsec.sk>
2021-11-16 22:25:50 +0100

    using only secrets

--- test-automation/copy-to-target.sh
+++ test-automation/copy-to-target.sh
@@ -2,12 +2,7 @@
 
 USER=copyuser
 TARGET=vm.napadovisko.sk:/etc/automation/
-PASSFILE=secrets/vm.napadovisko.sk-copyuser.pass.txt
-
-# inject client secret from a secret file (32a1d21f0de4e551cf80) to casc.yanl
-secret=$(<secrets/32a1d21f0de4e551cf80)
-cp jcasc/template-casc.yaml jcasc/casc.yaml
-sed -i "s/@@@@-client-secret-32a1d21f0de4e551cf80-@@@@/$secret/" jcasc/casc.yaml
+PASSFILE=secrets-local/vm.napadovisko.sk-copyuser.pass.txt
 
 
 folders=( jcasc nginx secrets )
@@ -15,7 +10,7 @@ folders=( jcasc nginx secrets )
 files=(
     jcasc
     nginx
-    secrets/
+    secrets
     docker-compose.yml
 )
 
@@ -29,5 +24,3 @@ do
     sshpass -f $PASSFILE scp -r ./$f $USER@$TARGET
 done
 
-# remove file with secret
-rm jcasc/casc.yaml
